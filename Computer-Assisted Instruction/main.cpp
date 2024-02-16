3907451914a38452ab27d4cd0557b9585b30e398
Hannah Ashton <h.charise18@gmail.com>
2024-02-16 15:38:58 -0700

    Initial Commit

---
+++ Computer-Assisted Instruction/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Computer-Assisted Instruction
+//
+//  Created by Hannah Ashton on 2/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
